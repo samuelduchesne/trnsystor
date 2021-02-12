948f5f3da987b6b917abeb3edb40b4915f93c1db
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-02-12 02:24:56 -0500

    D205

--- trnsystor/component.py
+++ trnsystor/component.py
@@ -285,6 +285,7 @@ class Component(metaclass=ABCMeta):
     @property
     def is_connected(self):
         """Whether or not this Component is connected to another TypeVariable.
+
         Connected to or connected by.
         """
         return any([len(list(self.predecessors)) > 0, len(list(self.successors)) > 0])
