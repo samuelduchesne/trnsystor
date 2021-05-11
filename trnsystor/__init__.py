b84757af59263ef212bd655b9b3767076248b225
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2021-05-11 10:11:44 -0400

    adds version scm

--- trnsystor/__init__.py
+++ trnsystor/__init__.py
@@ -2,14 +2,29 @@
 
 from outdated import warn_if_outdated
 
+from .collections import EquationCollection
 from .controlcards import ControlCards
 from .deck import Deck
+from .statement import Equation
 from .trnsysmodel import TrnsysModel
 
 # Version of the package
-__version__ = "1.3.2"
+from pkg_resources import get_distribution, DistributionNotFound
 
-# warn if a newer version of archetypal is available
-warn_if_outdated("trnsystor", __version__)
+try:
+    __version__ = get_distribution("archetypal").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "0.0.0"  # should happen only if package is copied, not installed.
+else:
+    # warn if a newer version of trnsystor is available
+    from outdated import warn_if_outdated
 
-__all__ = ["ControlCards", "Deck", "TrnsysModel", "__version__"]
+__all__ = [
+    "Equation",
+    "EquationCollection",
+    "ControlCards",
+    "Deck",
+    "TrnsysModel",
+    "__version__",
+]
