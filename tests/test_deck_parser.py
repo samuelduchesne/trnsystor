1acd51d242925298884b38c158fb67365a2c79a5
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2025-08-16 07:20:25 -0400

    Add reentrancy test for deck parsing

---
+++ tests/test_deck_parser.py
@@ -0,0 +1,22 @@
+from path import Path
+
+from trnsystor.deck import Deck
+from trnsystor.component import Component
+import itertools
+import networkx as nx
+
+
+def _parse_deck():
+    Component.INIT_UNIT_NUMBER = itertools.count(start=1)
+    Component.UNIT_GRAPH = nx.MultiDiGraph()
+    return Deck.read_file(
+        Path("tests/input_files/test_deck.dck"), proforma_root="tests/input_files"
+    )
+
+
+def test_parse_is_reentrant():
+    first = _parse_deck()
+    second = _parse_deck()
+    assert [type(m).__name__ for m in first.models] == [
+        type(m).__name__ for m in second.models
+    ]
