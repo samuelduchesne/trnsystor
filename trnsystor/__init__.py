4eff2d31efff3178327d892bfc139420f60c3347
Samuel Letellier-Duchesne <samuelduchesne@me.com>
2025-08-16 08:16:14 -0400

    chore: add development tooling

--- trnsystor/__init__.py
+++ trnsystor/__init__.py
@@ -1,4 +1,8 @@
-"""Trnsystor Module."""
+"""Public package interface."""
+
+from __future__ import annotations
+
+from importlib import metadata
 
 from outdated import warn_if_outdated
 
@@ -8,18 +12,6 @@ from .deck import Deck
 from .statement import Equation
 from .trnsysmodel import TrnsysModel
 
-# Version of the package
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    __version__ = get_distribution("archetypal").version
-except DistributionNotFound:
-    # package is not installed
-    __version__ = "0.0.0"  # should happen only if package is copied, not installed.
-else:
-    # warn if a newer version of trnsystor is available
-    from outdated import warn_if_outdated
-
 __all__ = [
     "Equation",
     "EquationCollection",
@@ -28,3 +20,10 @@ __all__ = [
     "TrnsysModel",
     "__version__",
 ]
+
+try:
+    __version__ = metadata.version("trnsystor")
+except metadata.PackageNotFoundError:  # pragma: no cover - fallback for local runs
+    __version__ = "0.0.0"
+else:  # pragma: no branch
+    warn_if_outdated("trnsystor", __version__)
